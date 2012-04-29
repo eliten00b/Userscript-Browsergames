fc10c9ff2bf5dbbe7b398c092b5e988643abde89
EliteN00b <bfr-egneg@hotmail.de>
2012-04-29 11:01:17 +0200

    init commmit T4_MP

---
+++ t4_mp.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name           Travian+ Multiplayer
+// @namespace      TravainMP
+// @version        0.1
+// @description    Enable Multiplayer for Travian 4.0
+// @include        http://t*.travian.de/*
+// @exclude        http://*.travian.de/login.php
+// ==/UserScript==
+
+if(!TravExtension) { TravExtension = {} }
+
+TravExtension.MP = {
+  init = function() {}
+}
