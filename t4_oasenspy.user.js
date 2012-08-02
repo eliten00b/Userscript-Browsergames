e18c3dd5517fc9d727ec1021a9ad3fc5f85b329e
EliteN00b <bfr-egneg@hotmail.de>
2012-08-03 00:30:55 +0200

    oasesspy 0.2
    search button is working now

--- t4_oasenspy.user.js
+++ t4_oasenspy.user.js
@@ -1,15 +1,17 @@
 // ==UserScript==
 // @name           Travian+ Oasesspy
 // @namespace      TravainOasesspy
-// @version        0.1
+// @version        0.2
 // @description    Spy all oases and check for animals.
 // @include        http://t*.travian.de/karte.php*
 // ==/UserScript==
 
 T4 = function() {
   TE.Addons.Oasesspy = {
+    oases: [],
+
     init: function() {
-      TE.Utils.log('Oasesspy.init')
+      TE.Utils.log('Oasesspy start...')
 
       this.addButtons()
       this.prepareTable()
@@ -28,11 +30,65 @@ T4 = function() {
     },
 
     searchOases: function() {
+      var data = {
+            data: {
+              cmd: 'mapPositionData',
+              data: {
+                x: TE.Plus.Village.currentVillage().x,
+                y: TE.Plus.Village.currentVillage().y,
+                zoomLevel: 3
+              }
+            },
+            onSuccess: TE.Addons.Oasesspy.handleSearchSuccess,
+            url: '/ajax.php?cmd=mapPositionData'
+          }
 
+      TE.Utils.log('Start getting data for oases...')
+      TE.Utils.log(['Oasesspy.searchOases send', data], 2)
+      Travian.ajax(data)
     },
 
     spyOases: function() {
+      var data = {
+            data: {
+              cmd: 'viewTileDetails',
+              data: {
+                x: 0,
+                y: 0
+              }
+            },
+            onSuccess: TE.Addons.Oasesspy.handleSuccess,
+            url: '/ajax.php?cmd=viewTileDetails'
+          }
 
+    },
+
+    handleSearchSuccess: function(data) {
+      console.log(data, data.tiles)
+      var tiles = data.tiles
+        , tile
+
+      for(var i = 0; i < tiles.length; ++i) {
+        var tile = tiles[i]
+
+        if(typeof tile.c !== 'undefined') {
+          if(tile.c.match(/\{k\.fo\}/) !== null) {
+
+          } else {
+            delete tiles[i]
+          }
+        } else {
+          delete tiles[i]
+        }
+      }
+
+      tiles.erase(undefined)
+      TE.Utils.log('All oases successful get.')
+      TE.Addons.Oasesspy.oases = tiles
+    },
+
+    handleSpySuccess: function(data) {
+      var html = data.html
     }
   }
 
